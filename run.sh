521a71f5c1c39fd55673fc91762583926473860d
🐨♥️🍻 <jono@bowerswilkins.com>
2017-04-25 11:38:15 -0700

    updated to 5.3.1

--- run.sh
+++ run.sh
@@ -1,20 +1,9 @@
-#!/bin/sh
-
-set -e
+#!/bin/sh -ex
 
 KIBANA_CONF_FILE="/opt/kibana-${KIBANA_VERSION}/config/kibana.yml"
 
-KIBANA_ES_URL=${KIBANA_ES_URL:-http://elasticsearch:9200}
+ELASTICSEARCH_URL=${ELASTICSEARCH_URL:-http://elasticsearch:9200}
 
-sed -i "s;^# elasticsearch_url:.*;elasticsearch_url: ${KIBANA_ES_URL};" "${KIBANA_CONF_FILE}"
-
-if [ -n "${KIBANA_INDEX}" ]; then
-    echo "setting index!"
-    sed -i "s;^kibana_index:.*;kibana_index: ${KIBANA_INDEX};" "${KIBANA_CONF_FILE}"
-fi
-
-# mesos-friendly change
-unset HOST
-unset PORT
+sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" "${KIBANA_CONF_FILE}"
 
 exec /opt/kibana-${KIBANA_VERSION}/bin/kibana
